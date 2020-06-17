c405787189ccb636bf3e2e1fc46110980aceec8f
Jonathan Anjos <jonathan.d.anjos@accenture.com>
2020-06-17 14:08:45 -0300

    refactor: renaming screen responsible file

--- processes/screen/index.js
+++
@@ -1,8 +0,0 @@
-const { ASPECTRATIO } = require("../../constants");
-
-const configureGameCanvasDimensions = (canvas) => {
-  canvas.height = window.innerHeight;
-  canvas.width = canvas.height * ASPECTRATIO;
-};
-
-export { configureGameCanvasDimensions };
