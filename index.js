55595905ea33fb80156049adbc7851b5fd8a3577
Jonathan Anjos <jonathan.d.anjos@accenture.com>
2020-06-17 11:47:57 -0300

    feature: Adding walk animation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import spritesImage from "./assets/images/sprites.png";
-import { KEY } from "./constants";
+import { KEY, TICKS_PER_FRAME } from "./constants";
 const { configureGameCanvasDimensions } = require("./processes/screen");
 
 const canvas = document.getElementById("game-canvas");
@@ -32,7 +32,6 @@ function engine() {
   );
 
   function onkey(ev, key, pressed) {
-    console.log(player);
     switch (key) {
       case KEY.LEFT:
         player.input.left = pressed;
@@ -54,10 +53,10 @@ function engine() {
   startGame();
 
   function startGame() {
-    gameLoop();
+    gameLoop(0);
   }
 
-  function gameLoop() {
+  function gameLoop(frame) {
     update();
     render(player);
     requestAnimationFrame(gameLoop);
@@ -69,17 +68,30 @@ function update() {}
 function render(player) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (player.input.left) {
-    player.position.x -= 5;
-  }
-  if (player.input.right) {
-    player.position.x += 5;
-  }
-  if (player.input.jump) {
-    player.position.y += 5;
-  }
+  if (player.hasInputPressed()) {
+    if (player.input.left) {
+      player.position.x -= 5;
+      if (player.position.x < 0) player.position.x = 0;
+    }
 
-  player.drawPlayerStand();
+    if (player.input.right) {
+      player.position.x += 5;
+      if (
+        player.position.x >=
+        canvas.width - player.sprtStand.width * player.sprtScale
+      )
+        player.position.x =
+          canvas.width - player.sprtStand.width * player.sprtScale;
+    }
+
+    if (player.input.jump) {
+      player.position.y += 5;
+    }
+
+    player.drawPlayerWalk();
+  } else {
+    player.drawPlayerStand();
+  }
 }
 
 function Player(ctx) {
@@ -88,33 +100,69 @@ function Player(ctx) {
     height: 16,
     frames: 3,
   };
-
   this.input = {
     left: false,
     right: false,
     jump: false,
   };
-
+  this.hasInputPressed = () => {
+    const { left, right, jump } = this.input;
+    return left || right || jump;
+  };
   this.position = {
     x: 0,
     y: 0,
   };
+  this.tickCount_walk = 1;
+  this.frameIndex_walk = 1;
+
+  this.resetTickCount = () => {
+    this.tickCount_walk = 0;
+    this.frameIndex_walk = 1;
+  };
+
+  // The number of times that sprite will be scaled. Later, this value will change according to user screen size
+  this.sprtScale = 5;
 
   return {
     input: this.input,
     position: this.position,
-    drawPlayerStand: (frame = 0) => {
+    sprtStand: this.sprtStand,
+    sprtScale: this.sprtScale,
+    hasInputPressed: this.hasInputPressed,
+    drawPlayerStand: () => {
+      this.resetTickCount();
       ctx.drawImage(
         megamanSprite,
-        this.sprtStand.width * frame,
-        194,
+        0,
+        193,
         this.sprtStand.width,
         this.sprtStand.height,
         this.position.x,
         this.position.y,
-        this.sprtStand.width * 5,
-        this.sprtStand.height * 5
+        this.sprtStand.width * this.sprtScale,
+        this.sprtStand.height * this.sprtScale
       );
+    },
+    drawPlayerWalk: () => {
+      this.tickCount_walk++;
+      if (this.tickCount_walk > TICKS_PER_FRAME) {
+        this.tickCount_walk = 0;
+        this.frameIndex_walk++;
+        if (this.frameIndex_walk > 3) this.frameIndex_walk = 1;
+      }
+      ctx.drawImage(
+        megamanSprite,
+        this.sprtStand.width * this.frameIndex_walk,
+        193,
+        this.sprtStand.width,
+        this.sprtStand.height,
+        this.position.x,
+        this.position.y,
+        this.sprtStand.width * this.sprtScale,
+        this.sprtStand.height * this.sprtScale
+      );
+      this.tickCount++;
     },
   };
 }
